825f5e81fd339cee15efba91e6505617c963e393
sontran-q7 <anhsontran.q7@gmail.com>
2023-12-08 21:41:03 +0700

    navbar1

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 // Navbar.jsx
-
 import React from 'react';
 import './Navbar.css';
 
@@ -10,25 +9,26 @@ const Navbar = () => {
       <div className="navbar-container">
         <div className="navbar">
           <div className="logo-container">
-            <img src="/path/to/your/logo.png" alt="Logo" className="logo"  />  
+            <img src=".\image\Free_Sample_By_Wix.jpg" alt="Madam Boutique" className="logo" width="80px" height="35px" />  
           </div>
         </div>
 
         <div className="search-cart-container">
-            <input type="text" placeholder="Search" className="search-input" />
-            
-          </div>
+          <input type="text" placeholder="Search" className="search-input" />
+        </div>
       </div>
 
       {/* Navbar 2 */}
       <div className="navbar-container2">
         <div className="navbar">
-          <a href="#">Giới Thiệu</a>
-          <a href="#">Sản Phẩm</a>
-          <a href="#">Phụ Kiện</a>
-          <a href="#">Bộ Sưu Tập</a>
-          <a href="#">Liên Hệ</a>
-          <a href="#">Giỏ Hàng</a>
+          <ul className="menu_main">
+            <li><a href="#">Giới Thiệu</a></li>
+            <li><a href="#">Sản Phẩm</a></li>
+            <li><a href="#">Phụ Kiện</a></li>
+            <li><a href="#">Bộ Sưu Tập</a></li>
+            <li><a href="#">Liên Hệ</a></li>
+            <li><a href="#">Giỏ Hàng</a></li>
+          </ul>
         </div>
       </div>
     </div>
