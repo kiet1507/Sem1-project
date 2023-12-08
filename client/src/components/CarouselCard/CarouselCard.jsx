3e0c1a64d04ca30658432c3454653584f51a0819
Nguyễn Hữu Đức Vũ <ducvuglotec@gmail.com>
2023-12-08 21:45:32 +0700

    gallery components

---
+++ client/src/components/CarouselCard/CarouselCard.jsx
@@ -0,0 +1,137 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from "react";
+import './CarouselCard.scss';
+import _items from './data_fake.json';
+
+const slideWidth = 30;
+
+const length = _items.length;
+_items.push(..._items);
+
+const sleep = (ms = 0) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const createItem = (position, idx) => {
+    const item = {
+        styles: {
+            transform: `translateX(${position * slideWidth}rem)`,
+        },
+        player: _items[idx].player,
+    };
+
+    switch (position) {
+        case length - 1:
+        case length + 1:
+            item.styles = { ...item.styles, filter: 'grayscale(1)' };
+            break;
+        case length:
+            break;
+        default:
+            item.styles = { ...item.styles, opacity: 0 };
+            break;
+    }
+
+    return item;
+};
+
+const CarouselSlideItem = ({ pos, idx, activeIdx }) => {
+    const item = createItem(pos, idx, activeIdx);
+
+    return (
+        <li className="carousel__slide-item" style={item.styles}>
+            <div className="carousel__slide-item-img-link">
+                <img src={item.player.image} alt={item.player.title} />
+            </div>
+            <div className="carousel-slide-item__body">
+                <h4>{item.player.title}</h4>
+                <p>{item.player.desc}</p>
+            </div>
+        </li>
+    );
+};
+
+const keys = Array.from(Array(_items.length).keys());
+
+const CarouselCard = () => {
+    const [items, setItems] = useState(keys);
+    const [isTicking, setIsTicking] = useState(false);
+    const [activeIdx, setActiveIdx] = useState(0);
+    const bigLength = items.length;
+
+    const prevClick = (jump = 1) => {
+        if (!isTicking) {
+            setIsTicking(true);
+            setItems((prev) => {
+                return prev.map((_, i) => prev[(i + jump) % bigLength]);
+            });
+        }
+    };
+
+    const nextClick = (jump = 1) => {
+        if (!isTicking) {
+            setIsTicking(true);
+            setItems((prev) => {
+                return prev.map(
+                    (_, i) => prev[(i - jump + bigLength) % bigLength],
+                );
+            });
+        }
+    };
+
+    const handleDotClick = (idx) => {
+        if (idx < activeIdx) prevClick(activeIdx - idx);
+        if (idx > activeIdx) nextClick(idx - activeIdx);
+    };
+
+    useEffect(() => {
+        if (isTicking) sleep(300).then(() => setIsTicking(false));
+    }, [isTicking]);
+
+    useEffect(() => {
+        setActiveIdx((length - (items[0] % length)) % length) // prettier-ignore
+    }, [items]);
+
+    return (
+        <div className="carousel__wrap">
+            <div className="carousel__inner">
+                <button className="carousel__btn carousel__btn--prev" onClick={() => prevClick()}>
+                    <i className="carousel__btn-arrow carousel__btn-arrow--left" />
+                </button>
+                <div className="carousel__container">
+                    <ul className="carousel__slide-list">
+                        {items.map((pos, i) => (
+                            <CarouselSlideItem
+                                key={i}
+                                idx={i}
+                                pos={pos}
+                                activeIdx={activeIdx}
+                            />
+                        ))}
+                    </ul>
+                </div>
+                <button className="carousel__btn carousel__btn--next" onClick={() => nextClick()}>
+                    <i className="carousel__btn-arrow carousel__btn-arrow--right" />
+                </button>
+                <div className="carousel__dots">
+                    {items.slice(0, length).map((pos, i) => (
+                        <button
+                            key={i}
+                            onClick={() => handleDotClick(i)}
+                            className={i === activeIdx ? 'dot active' : 'dot'}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+CarouselSlideItem.propTypes = {
+    pos: PropTypes.number.isRequired,
+    idx: PropTypes.number.isRequired,
+    activeIdx: PropTypes.number.isRequired,
+};
+
+export default CarouselCard;
+
